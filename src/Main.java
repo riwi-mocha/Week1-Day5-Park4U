20aa786b60c819a761e3b6b9488d64cb0445ea9c
Johan192004 <johanruma@gmail.com>
2025-09-12 20:10:05 -0500

    Add initial implementation of Main and Vehicle classes

---
+++ src/Main.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // 1. Entrance of vehicles
+        ArrayList<Vehicle> vehiclesParked = new ArrayList<>();
+
+        // 4. Box
+
+        int ticketsCar = 0;
+        int finesCar = 0;
+        int discountsCar = 0;
+        int surchargesCar = 0;
+
+        int ticketsMotorcycle = 0;
+        int finesMotorcycle = 0;
+        int discountsMotorcycle = 0;
+        int surchargesMotorcycle = 0;
+
+        int ticketsTruck = 0;
+        int finesTruck = 0;
+        int discountsTruck = 0;
+        int surchargesTruck = 0;
+
+        int totalShift = 0;
+
+
+        // 5. reports
+
+        double[] hoursStayedCar = {};
+        double[] hoursStayedMotorcycle = {};
+        double[] hoursStayedTruck = {};
+
+        int levelOneAvailable = 30;
+        int levelTwoAvailable = 40;
+        int levelThreeAvailable = 40;
+        int levelFourAvailable = 50;
+
+    }
+}
